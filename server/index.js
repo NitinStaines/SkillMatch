0e7d19d14d1e4eb3f70642e88a606a3f038c2675
Nitin Staines <nitin2210274@ssn.edu.in>
2025-06-17 22:56:41 +0530

    Initial setup with backend and frontend. Created User model and also created Authentication routes.

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);  
+
+app.get('/', (req, res) => {
+    res.send("API works");
+})
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => console.log('MongoDB connection error:', err));
+
+
+
+  
