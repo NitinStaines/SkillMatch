1fc43cbc367816adb28ff07d7cc2117d1a26cc6b
Nitin Staines <nitin2210274@ssn.edu.in>
2025-08-21 14:59:45 +0530

    Chatrooms styled

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,7 +15,7 @@ router.post('/register', async (req, res) => {
     const {name, email, password} = req.body;
 
     const existingUser = await User.findOne({email});
-    if (existingUser){
+    if (existingUser){  
         return res.status(400).json({message: "User already exists"});
     }
     const hashedPassword = await bcrypt.hash(password, 10);
